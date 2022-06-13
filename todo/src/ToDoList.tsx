c687a90806a7401864d214507acb480cd08999ab
insidelamp <rkdhfh@naver.com>
2022-06-13 14:03:02 +0900

    리액트폼 적용 예시

--- todo/src/ToDoList.tsx
+++ todo/src/ToDoList.tsx
@@ -1,21 +1,19 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 function ToDoList() {
-  const [toDo, setToDo] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setToDo(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(toDo);
-  };
+  const { register, watch } = useForm();
+  console.log(watch());
+
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} placeholder="Write a to do" />
+      <form>
+        <input {...register("email")} placeholder="Email"></input>
+        <input {...register("email")} placeholder="Email"></input>
+        <input {...register("email")} placeholder="Email"></input>
+        <input {...register("email")} placeholder="Email"></input>
+        <input {...register("email")} placeholder="Email"></input>
+        <input {...register("email")} placeholder="Email"></input>
         <button>Add</button>
       </form>
     </div>
